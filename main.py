2cebb2e2a05064c1e7d13c4b5c4a8c1026aaef3b
Alireza Saei <saeialireza359@gmail.com>
2022-12-31 13:08:18 +0330

    added exception tracing

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import ultrasonic
 import pyb
 import time
+import micropython
+
+# last exception is tracable by code below (stack trace size == 1)
+micropython.alloc_emergency_exception_buf(100)
 
 
 def calc_distance():
@@ -21,7 +25,7 @@ sensor_trig_pin = pyb.Pin.board.X3
 sensor_echo_pin = pyb.Pin.board.X4
 sensor = ultrasonic.Ultrasonic(sensor_trig_pin, sensor_echo_pin)
 
-# counter increases from 0 to 15 every second and then resets (4-bit counter)
+# counter increases from 0 to 15 (1 every second) and then resets (4-bit counter)
 counter = 0
 while True:
     if (counter == 16):
